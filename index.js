ba0472e399ddf495d4ab24bd5df0a479dcce3f76
Sifat2245 <Sifat2245@users.noreply.github.com>
2025-06-10 23:17:50 +0600

    storing user info in database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000
 
 
@@ -9,7 +11,34 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qwhtqkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+const run = async() =>{
+    try{
+        const userCollection = client.db('Asian_Restaurant').collection('users')
+
+        app.post('/users', async(req,res) =>{
+            const newUser = req.body
+            const result = await userCollection.insertOne(newUser)
+            res.send(result)
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Asians sever')
