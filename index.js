ada5add994859a14cbcca1ae7421ee87b33b65fc
Sifat2245 <Sifat2245@users.noreply.github.com>
2025-06-13 15:21:02 +0600

    craeted api for post data and get ordereddetails data. and also update purchase count and quantity from specific food item on each order

--- index.js
+++ index.js
@@ -14,60 +14,88 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qwhtqkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
-const run = async() =>{
-    try{
+const run = async () => {
+    try {
         const userCollection = client.db('Asian_Restaurant').collection('users')
         const foodsCollection = client.db('Asian_Restaurant').collection('foods')
+        const orderCollection = client.db('Asian_Restaurant').collection('orders')
 
-        app.post('/users', async(req,res) =>{
+        app.post('/users', async (req, res) => {
             const newUser = req.body
             const result = await userCollection.insertOne(newUser)
             res.send(result)
         })
 
-        app.get('/users', async(req, res) =>{
+        app.get('/users', async (req, res) => {
             const users = req.body
-            const  result = await userCollection.find(users).toArray()
+            const result = await userCollection.find(users).toArray()
             res.send(result)
         })
 
-        app.post('/foods', async(req, res) =>{
+        app.post('/foods', async (req, res) => {
             const foods = req.body
             const result = await foodsCollection.insertOne(foods)
             res.send(result)
         })
 
-        app.get('/foods', async(req, res) =>{
+        app.patch('/foods/:id', async(req,res) =>{
+            const foodId = req.params.id
+            const {orderedQuantity} = req.body
+            const result = await foodsCollection.updateOne(
+                {_id: new ObjectId(foodId)},
+                {
+                    $inc:{
+                        purchaseCount:(orderedQuantity),
+                        quantity: -(orderedQuantity)
+                    }
+                }
+            );
+            res.send(result)
+        })
+
+        app.get('/foods', async (req, res) => {
             const foods = req.body
             const result = await foodsCollection.find(foods).toArray()
             res.send(result)
         })
-        app.get('/foods/:id', async(req, res) =>{
+        app.get('/foods/:id', async (req, res) => {
             const id = req.params.id
-            const query = { _id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await foodsCollection.findOne(query)
             res.send(result)
         })
 
-        app.get('/top-purchase', async(req, res) =>{
+        app.get('/top-purchase', async (req, res) => {
             const result = await foodsCollection
-                    .find()
-                    .sort({purchaseCount: -1})
-                    .limit(8)
-                    .toArray()
-            res.send(result)        
+                .find()
+                .sort({ purchaseCount: -1 })
+                .limit(8)
+                .toArray()
+            res.send(result)
+        })
+
+        app.post('/orders', async (req, res) => {
+            const orders = req.body
+            const result = await orderCollection.insertOne(orders)           
+            res.send(result)
+        })
+
+        app.get('/orders', async (req, res) => {
+            const orders = req.body
+            const result = await orderCollection.find(orders).toArray()
+            res.send(result)
         })
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -78,6 +106,6 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Asians sever')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('connected');
 })
